97786a4c7a4721258fcf457a157fb5f484b55bf4
Ab-350 <chaudrollen@gmail.com>
2025-01-30 19:08:08 +0100

    Delete fxmanifest.lua

--- fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'K3ISSI'
-description 'This disables every gta online type aircontrol and flipping car to wheels from upside down :D this is my first ever wannabe script so please, dont change the folder name'
-version '1.0'
-
-client_script 'client.lua'
